e476a0a79196cdb1e4c009ac61da41aff3da25d6
Navilur Rahman <navilur.rahman@gmail.com>
2025-05-29 02:03:51 +0600

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api/"
-    : "/api/";
+    : "https://mern-thikboard.onrender.com/api/";
 
 const api = axios.create({
   baseURL: BASE_URL,
