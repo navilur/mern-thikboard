a8d41d12a0b30198c05613a64ad39596ef36c377
Navilur Rahman <navilur.rahman@gmail.com>
2025-05-29 02:09:41 +0600

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api/"
-    : "https://mern-thikboard.onrender.com/api/";
+const BASE_URL = "https://mern-thikboard.onrender.com/api/";
+// import.meta.env.MODE === "development"
+//   ? "http://localhost:5001/api/"
+//   : "https://mern-thikboard.onrender.com/api/";
 
 const api = axios.create({
   baseURL: BASE_URL,
