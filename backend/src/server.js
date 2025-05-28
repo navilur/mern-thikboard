130aca7f5aa327f639ec34fae357bdcecfc7097b
Navilur Rahman <navilur.rahman@gmail.com>
2025-05-29 01:30:42 +0600

    production

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,22 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // this middleware will parse the JSON bodies: req.body
 app.use(rateLimiter);
 
@@ -26,6 +30,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (res, req) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
